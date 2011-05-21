76caa5a1da98c099fca13f08dd47478f48bcdbda
Kazuhiro Sera <seratch@gmail.com>
2011-05-21 15:59:26 +0900

    Added IO loan pattern utils

--- src/main/scala/com/github/seratch/scalikesolr/http/HttpClient.scala
+++ src/main/scala/com/github/seratch/scalikesolr/http/HttpClient.scala
@@ -2,6 +2,7 @@ package com.github.seratch.scalikesolr.http
 
 import java.net.{URL, HttpURLConnection}
 import java.io._
+import com.github.seratch.scalikesolr.util.IO
 
 object HttpClient {
 
@@ -15,24 +16,11 @@ object HttpClient {
     conn.setRequestProperty("Content-Type", contentType)
     conn.setRequestProperty("Content-Length", dataBinary.size.toString)
 
-    var os: OutputStream = null
-    var writer: OutputStreamWriter = null
-    try {
-      conn.setDoOutput(true);
-      os = conn.getOutputStream();
-      writer = new OutputStreamWriter(os, charset);
-      writer.write(dataBinary);
-    } finally {
-      writer match {
-        case null =>
-        case _ => try writer.close catch {
-          case _ =>
-        }
-      }
-      os match {
-        case null =>
-        case _ => try os.close catch {
-          case _ =>
+    conn.setDoOutput(true);
+    IO.using(conn.getOutputStream) {
+      os => {
+        IO.using(new OutputStreamWriter(os, charset)) {
+          writer => writer.write(dataBinary)
         }
       }
     }
@@ -56,40 +44,7 @@ object HttpClient {
   }
 
   private def getResponseContent(conn: HttpURLConnection, charset: String): String = {
-    var is: InputStream = null
-    var br: BufferedReader = null
-    try {
-      is = conn.getInputStream
-      val isr = charset match {
-        case null => new InputStreamReader(is)
-        case _ => new InputStreamReader(is, charset)
-      }
-      br = new BufferedReader(isr)
-      val buf = new StringBuilder
-      var line: String = null
-      while ( {
-        line = br.readLine;
-        line
-      } != null) {
-        buf.append(line)
-        buf.append("\n")
-      }
-      buf.toString
-    }
-    finally {
-      is match {
-        case null =>
-        case _ => try is.close catch {
-          case _ =>
-        }
-      }
-      br match {
-        case null =>
-        case _ => try br.close catch {
-          case _ =>
-        }
-      }
-    }
+    IO.readAsString(conn.getInputStream, charset)
   }
 
 }
---
+++ src/main/scala/com/github/seratch/scalikesolr/util/IO.scala
@@ -0,0 +1,46 @@
+package com.github.seratch.scalikesolr.util
+
+import java.io.{InputStreamReader, BufferedReader, InputStream}
+
+object IO {
+
+  def using[CLOSABLE <: {def close() : Unit}, RETURNED](resource: CLOSABLE)(func: CLOSABLE => RETURNED): RETURNED = {
+    try {
+      func(resource)
+    } finally {
+      resource match {
+        case null =>
+        case _ => try resource.close() catch {
+          case _ =>
+        }
+      }
+    }
+  }
+
+  def readAsString(is: InputStream, charset: String): String = {
+    using(is) {
+      is => {
+        using({
+          new BufferedReader(charset match {
+            case null => new InputStreamReader(is)
+            case _ => new InputStreamReader(is, charset)
+          })
+        }) {
+          br => {
+            val buf = new StringBuilder
+            var line: String = null
+            while ( {
+              line = br.readLine;
+              line
+            } != null) {
+              buf.append(line)
+              buf.append("\n")
+            }
+            buf.toString
+          }
+        }
+      }
+    }
+  }
+
+}
