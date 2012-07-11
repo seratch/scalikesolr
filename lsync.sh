91a180a208d94d2cf95afdf015297e6cb790e482
Kazuhiro Sera <seratch@gmail.com>
2012-07-12 07:34:20 +0900

    Added lsync script

---
+++ lsync.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "[usage] $0 [vesrion]"
+  exit 1
+fi
+
+VERSION=$1
+curl -X POST http://ls.implicit.ly/api/1/libraries \
+  -d 'user=seratch&repo=scalikesolr&version=$VERSION'
+
+echo "Done."
+
