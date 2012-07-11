2ea74481656b4362dc6350bbaedb2e77ade0f7c7
Kazuhiro Sera <seratch@gmail.com>
2012-07-11 11:11:41 +0900

    Updated project settings

--- project/Build.scala
+++ project/Build.scala
@@ -10,13 +10,51 @@ object ScalikeSolrBuild extends Build {
     organization := "com.github.seratch",
     name := "scalikesolr",
     version := "3.6.0",
+    scalaVersion := "2.9.2",
+    crossScalaVersions := Seq("2.9.2", "2.9.1", "2.9.0"),
+    libraryDependencies <++= (scalaVersion) { scalaVersion =>
+      Seq(
+        "org.slf4j"                % "slf4j-api"          % "1.6.4"           % "compile",
+        "joda-time"                % "joda-time"          % "1.6.2"           % "compile",
+        "org.apache.solr"          % "solr-solrj"         % "3.6.0"           % "compile",
+        "ch.qos.logback"           % "logback-classic"    % "1.0.0"           % "test",
+        "junit"                    % "junit"              % "4.10"            % "test",
+        "org.mockito"              % "mockito-all"        % "1.8.2"           % "test",
+        "org.scalatest"            %% "scalatest"         % "1.7.1"           % "test",
+        "org.scala-lang"           % "scala-compiler"     % scalaVersion      % "test"
+      )
+    },
+    externalResolvers ~= (_.filter(_.name != "Scala-Tools Maven2 Repository")),
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
         if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
         else Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
     publishMavenStyle := true,
-    scalacOptions ++= Seq("-deprecation", "-unchecked")
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
+    publishArtifact in Test := false,
+    pomIncludeRepository := { x => false },
+    pomExtra := (
+      <url>http://seratch.github.com/scalikesolr</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:seratch/scalikesolr.git</url>
+        <connection>scm:git:git@github.com:seratch/scalikesolr.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>seratch</id>
+          <name>Kazuhuiro Sera</name>
+          <url>http://seratch.net/</url>
+        </developer>
+      </developers>
+    )
   )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,22 +3,21 @@ externalResolvers ~= (_.filter(_.name != "Scala-Tools Maven2 Repository"))
 resolvers ++= Seq(
   Classpaths.typesafeResolver,
   "sbt-idea-repo" at "http://mpeltonen.github.com/maven/",
-  "less is" at "http://repo.lessis.me",
   "coda" at "http://repo.codahale.com")
 
-addSbtPlugin("com.github.seratch" %% "testgen-sbt" % "0.3.0")
+addSbtPlugin("com.github.seratch" %% "testgenerator" % "1.1.0")
 
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.0.0")
+//addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.0.0")
+libraryDependencies += Defaults.sbtPluginExtra("com.github.mpeltonen" % "sbt-idea" % "1.0.0", "0.11.2", "2.9.1")
 
-addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.1")
-
-addSbtPlugin("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.3.0")
-
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
+//addSbtPlugin("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.3.0")
+libraryDependencies += Defaults.sbtPluginExtra("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.3.0", "0.11.2", "2.9.1")
 
 // for sonatype publishment
 
 resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.5")
+//addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.5")
+libraryDependencies += Defaults.sbtPluginExtra("com.jsuereth" % "xsbt-gpg-plugin" % "0.5", "0.11.2", "2.9.1")
+
 
