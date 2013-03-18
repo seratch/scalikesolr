555833810a3620e03eb834b188b5225002391b66
Kazuhiro Sera <seratch@gmail.com>
2013-03-18 13:46:21 +0900

    version 4.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ScalikeSolrBuild extends Build {
     sbtPlugin := false,
     organization := "com.github.seratch",
     name := "scalikesolr",
-    version := "4.2.1-SNAPSHOT",
+    version := "4.2.1",
     scalaVersion := "2.10.0",
     crossScalaVersions := Seq("2.10.0"),
     libraryDependencies <++= (scalaVersion) { scalaVersion =>
