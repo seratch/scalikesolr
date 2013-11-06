84f417752f4cc778c86678019c27fdd6fe5b4149
Kazuhiro Sera <seratch@gmail.com>
2013-11-06 12:32:15 +0900

    Fixed script for sbt 0.13

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sbt ++2.10.0 publish "g scala2.9" ++2.9.2 publish ++2.9.3 publish
+sbt ++2.10.0 publish-signed "g scala2.9" ++2.9.2 publish-signed ++2.9.3 publish-signed
 git reset --hard
 
